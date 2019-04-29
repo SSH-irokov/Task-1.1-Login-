584c561e26eb11706bac7b28a617f07fa6d9a9e6
Pasha <pavel2014@mail.ru>
2019-04-29 14:01:12 +0300

    Initial Commit

---
+++ Task 1.1 (Segues)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Task 1.1 (Segues)
+//
+//  Created by Паша on 29/04/2019.
+//  Copyright © 2019 Паша. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
